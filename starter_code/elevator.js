372eda5ae68a0051dae770312385a13cf8441e7a
100fue <malvacien@gmail.com>
2018-01-29 17:53:54 +0100

    iteration 1 done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,48 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.position   = this.floor;
+    this.direction  = "up";
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    setInterval(update,1000);
+  }
+
+  stop() { 
+    let stopSetInterval = setInterval(update,1000);
+    clearInterval(stopSetInterval);
+  }
+  
+  update() { 
+    // console.log(this.floor += 1);
+  }
+
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+
+  floorUp() { 
+    if(this.floor == 0){
+      this.floor += 1;
+    }
+  }
+
+  floorDown() {
+    if(this.floor == this.MAXFLOOR){
+      this.floor -= 1;
+    }
+  }
+
+  call() {
+
+  }
+
+  log() {
+    console.log(this.position);
+    console.log(this.direction);
+  }
 }
 
 module.exports = Elevator;
+
+
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,9 @@
 const Elevator = require('./elevator.js');
+
+
+
+// function update(){
+//         console.log("hola");
+//   }
+  
+// setInterval(update,1000);
